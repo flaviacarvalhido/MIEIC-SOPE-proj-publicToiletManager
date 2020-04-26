a4d8873cc3cf2d6540fcbd45f38c0c6b2472d5bf
nuno <n.resende2000@gmail.com>
2020-04-26 15:40:15 +0100

    Trying to implement fifos

--- Q1.c
+++ Q1.c
@@ -6,6 +6,8 @@
 int main(int argc, char *argv[]){
 
     struct command c;
+    int fd;
+    char fifoname[100];
 
     c=parser(argc,argv);
 
@@ -14,6 +16,28 @@ int main(int argc, char *argv[]){
         printf("Usage: ./Q1 <-t nsecs> fifoname\n");
         exit(4);
     }
+
+
+    snprintf(fifoname, sizeof(fifoname), "/tmp/%s", c.fifoname);
+
+    printf("Making FIFO...\n");
+
+    mkfifo(fifoname, 0660);
+
+    printf("Opening...\n");
+
+    fd = open(fifoname, O_RDONLY, 00444);
+
+    printf("FD: %d\n", fd);
+
+    char data_received[100];
+
+    ssize_t bytes_read = read(fd, data_received, 100);
+
+    printf("data_received: %s\n", data_received);
+
+    close(fd);
+
     
     return 0;
 }
--- U1.c
+++ U1.c
@@ -1,9 +1,12 @@
 #include "register.h"
 #include "parser.h"
+#include <pthread.h>
 
 int main(int argc, char *argv[]){
 
     struct command c;
+    char fifoname[100];
+    int fd;
 
     c=parser(argc,argv);
 
@@ -12,5 +15,36 @@ int main(int argc, char *argv[]){
         printf("Usage: ./U1 <-t nsecs> fifoname\n");
         exit(4);
     }
+
+    snprintf(fifoname, sizeof(fifoname), "/tmp/%s", c.fifoname);
+
+
+    // lança continuamente (i.e. com intervalos de alguns milissegundos) threads, cada um ficando associado a um pedido;
+
+    // cada thread gera aleatoriamente a duração do acesso,
+    // e trata de toda a comunicação com o servidor (os pedidos são enviados ao servidor através do canal público fifoname); 
+    // as respostas do servidor são recebidas por um canal privado com nome, criado e posteriormente eliminado pelo cliente 
+    // termina só após o atendimento ter sido completado;
+
+
+    // [ i, pid, tid, dur, pl ]
+
+    do
+    {
+        fd = open(fifoname, O_WRONLY, 00222);
+        if (fd == -1) // Se ainda não tiver sido criado pelo reader
+            sleep(1);
+    } while (fd == -1);
+
+    printf("Got out of do while\n");
+
+    char fifo_data[100];
+
+    snprintf(fifo_data, sizeof(fifo_data), "[ %d, %d, %lu, %d, %d ]", 2, getpid(), pthread_self(), 3, 12);
+
+    write(fd, fifo_data, sizeof(fifo_data)); 
+
+    close(fd);
+    
     return 0;
 }
