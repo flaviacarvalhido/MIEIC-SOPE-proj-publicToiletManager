47a82dbe5d599dca8c07baef788dc2c9e8af8166
flavia <flaviacarvalhido@gmail.com>
2020-05-15 23:36:41 +0100

    está quase amigos, falta +tempo de clients

--- Q2.c
+++ Q2.c
@@ -8,29 +8,23 @@
 
 #define MAX_ATTEMPTS  5
 
-
 int fd_channels[10000];
 int is_free[10000];
 struct Queue * queue;
-
 sem_t nthread;
 sem_t nplacements;
-
-//int can_create = 0;
 int endProgram = 0;
-
-
-
 struct command c;
-
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER; 
+
+
+
 
 void *tooLate(void *arg)
 {
 
-
     char fifo_private[1000];
-    char response_string[100];
+    char response_string[1000];
     struct Request r = *(struct Request *)arg;
 
     snprintf(fifo_private, sizeof(fifo_private), "/tmp/%d.%lu", r.pid, r.tid);
@@ -64,13 +58,13 @@ void *processClient(void *arg)
     pthread_detach(pthread_self());
 
     char fifo_private[1000];
-
     int attempts = 0;
-
     struct Request r = *(struct Request *)arg;
+    char response_string[1000];
+    
 
-    
-    
+
+
     snprintf(fifo_private, sizeof(fifo_private), "/tmp/%d.%lu", r.pid, r.tid);
 
     do
@@ -90,12 +84,13 @@ void *processClient(void *arg)
     } while (fd_channels[r.request_number] == -1);
 
     
+    //placement na casa de banho
     sem_wait(&nplacements);
     pthread_mutex_lock(&mut);
     r.placement = dequeue(queue);
     pthread_mutex_unlock(&mut);
     
-    char response_string[100];
+   
 
     snprintf(response_string, sizeof(response_string), "[ %d, %d, %lu, %d, %d ]", r.request_number, getpid(), pthread_self(), r.duration, r.placement);
 
@@ -106,8 +101,8 @@ void *processClient(void *arg)
 
     writeRegister(r.request_number, getpid(), pthread_self(), r.duration, r.placement, ENTER); //se entrar
 
-    // Tá no processo de ir à casinha
 
+    // Tá no processo de ir à casinha
     usleep(r.duration*100000);
 
     // Time's up!
@@ -116,14 +111,13 @@ void *processClient(void *arg)
     sem_post(&nthread);
     sem_post(&nplacements);
 
+    //libertar casa de banho
     pthread_mutex_lock(&mut);
     enqueue(queue, r.placement);
     pthread_mutex_unlock(&mut);
 
 
     close(fd_channels[r.request_number]);
-
-    
 
     remove(fifo_private);
     pthread_exit(NULL);
@@ -133,7 +127,15 @@ void *processClient(void *arg)
 
 int main(int argc, char *argv[])
 {
-    
+
+    int fd;
+    char fifoname[1000];
+    pthread_t thread;
+    char data_received[1000];
+    struct Request r;
+
+
+    //incializar alarme
     struct sigaction act_alarm;
     act_alarm.sa_handler = sigalarm_handler;
     sigemptyset(&act_alarm.sa_mask);
@@ -145,13 +147,7 @@ int main(int argc, char *argv[])
     }  
 
 
-    int fd;
-    char fifoname[1000];
-    pthread_t thread;
-    char data_received[1000];
-    struct Request r;
-
-    
+    //parse command line
     c = parser(argc, argv);
 
     if (c.error)
@@ -161,13 +157,18 @@ int main(int argc, char *argv[])
         exit(4);
     }
 
+
+    //inicializar semáforo
     sem_init(&nthread,0,c.nthreads);
     sem_init(&nplacements,0,c.nplaces);
 
+    //inicializar queue
     queue = createQueue(c.nplaces);
     fillQueue(queue);
 
 
+
+    //abrir fifo público
     snprintf(fifoname, sizeof(fifoname), "/tmp/%s", c.fifoname);
 
     mkfifo(fifoname, 0660);
@@ -175,7 +176,7 @@ int main(int argc, char *argv[])
     fd = open(fifoname, O_RDONLY, 00444);
 
 
-
+    //inicializa tempo de programa
     alarm(c.nsecs);
 
     // Receives requests until closed
@@ -190,11 +191,8 @@ int main(int argc, char *argv[])
 
             writeRegister(r.request_number, getpid(), pthread_self(), r.duration, r.placement, RECVD);
 
-            //threads - uma para cada cliente
-            //2ndStage - número de threads limitadas
-
             
-            //sem_wait
+            //threads limitadas -n
             sem_wait(&nthread);
             pthread_create(&thread, NULL, processClient, (void *)&r);
 
@@ -204,10 +202,11 @@ int main(int argc, char *argv[])
     }
 
 
-    usleep(1000000);
 
     printf("Fechou mano\n");
 
+
+    //tratar dos pedidos depois do encerramento
     int i = 0;
 
     while ((i = read(fd, data_received, sizeof(data_received))) > 0 || (queue->size != c.nplaces))
@@ -222,20 +221,15 @@ int main(int argc, char *argv[])
             pthread_join(thread, NULL);
         }
         i=0;
+        
     }
 
     printf("Left while\n");
 
-    /*while(queue->size != c.nplaces){
-        usleep(1000);
-    }*/
-
     usleep(1000000);
    
 
-    
-    //pthread_join(last_one, NULL);
-
+    //finishing touches
     pthread_mutex_destroy(&mut);
 
     close(fd);
